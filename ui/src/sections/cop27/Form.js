0ee4665dcb144f27d4116a5d308660fb4394bf2a
Shikhar Bhatt <shikhar.bhatt011@gmail.com>
2022-11-07 02:20:06 +0100

    add cards and calc

--- ui/src/sections/cop27/Form.js
+++ ui/src/sections/cop27/Form.js
@@ -1,26 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 // @mui
 import { Link, Stack, IconButton, InputAdornment, TextField, Checkbox, Autocomplete, Popper, Select, MenuItem,
-  Button, ButtonGroup
+  Button, ButtonGroup, Card, Item
 } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import { LoadingButton } from '@mui/lab';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 // components
 import { styled } from '@mui/material/styles';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
+import WorkIcon from '@mui/icons-material/Work';
+import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import Divider from '@mui/material/Divider';
+import FactoryIcon from '@mui/icons-material/Factory';
+import NavigationIcon from '@mui/icons-material/Navigation';
+import MoneyIcon from '@mui/icons-material/Money';
 import Iconify from '../../components/iconify';
 import { airports } from '../../utils/airportLists';
-
+import { calculateFlightDistance } from './Calculator';
 
 const StyledPopper = styled((props) => <Popper placement="bottom-start" {...props} />)({
   width: '300px !important',
 });
 
-export default function Form() {
+export default function Form(props) {
   const navigate = useNavigate();
 
-  const [showPassword, setShowPassword] = useState(false);
   const [departure, setDeparture] = useState();
 	const [flightClass, setFlightClass] = useState('economy');
 	const [roundTrip, setRoundTrip] = useState(true);
@@ -28,6 +40,17 @@ export default function Form() {
 	const [flightDistance, setFlightDistance] = useState(0);
 	const [flightEmission, setFlightEmission] = useState();
 
+
+	useEffect(() => {
+    async function getDetails(departure, roundTrip, flightClass) {
+      const data = await calculateFlightDistance(departure, roundTrip, flightClass);
+      setFlightDistance(data.distance);
+      setFlightEmission(data.emission);
+    }
+		if (departure && flightClass && passengers) {
+		 getDetails(departure, roundTrip, flightClass)
+		}
+  }, [departure, flightClass, roundTrip, passengers]);
   const handleIncrement = () => {
 		const counter = passengers + 1
 		setPassengers(counter)
@@ -41,9 +64,23 @@ export default function Form() {
 	}
   return (
     <>
+    <Grid container spacing={4} direction="row" alignItems="center"
+  justifyContent="center">
+      <Grid item xs={10} sm={10} lg={6} >
+      <Card
+    sx={{
+      p: 5,
+      pt: 1,
+      m: 2,
+      boxShadow: '10px 5px 5px green',
+      // textAlign: 'center',
+      // alignContent: 'center',
+    }}
+  > 
+    <h3>Configure Flight</h3>
       <Stack spacing={3}>
       <Autocomplete
-          sx={{ width: 300 }}
+          sx={{ width: 300, mt: 2 }}
           autoHighlight
           popupIcon={null}
           PopperComponent={StyledPopper}
@@ -118,6 +155,68 @@ export default function Form() {
           <Button width="15%" color="success" onClick={handleIncrement} sx={{ width: '15%' }}><AddIcon /></Button>
         </ButtonGroup>
       </Stack>
+      </Card>
+      </Grid>
+      <Grid item xs={10} lg={6} sm={10}>
+        <Card
+    sx={{
+      p: 5,
+      pt: 1,
+      m: 2,
+      boxShadow: '10px 5px 5px blue',
+    }}
+  > 
+    <h3>Offset Flight</h3>
+      <Stack spacing={6}>
+      
+      {/* <p>Flight Distance: 100km</p>
+      <p>Flight Distance: 100km</p>
+      <p>Flight Distance: 100km</p> */}
+      <List
+      sx={{
+        width: '100%',
+        maxWidth: 360,
+        bgcolor: 'background.paper',
+      }}
+    >
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <NavigationIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={`${flightDistance || '---'} km`} secondary="Distance(one-way)" />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <FactoryIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={`${flightEmission?.asFloat() * passengers || '---'} tCO2`} secondary="Emissions" />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <MoneyIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="5" secondary="MATIC" />
+      </ListItem>
+    </List>
+      </Stack>
+
+      <Stack direction="row" alignItems="center" justifyContent="space-between" sx={{ my: 2 }}>
+        <Button color="secondary"  variant="contained"onClick={handleDecrement} >Pay</Button>
+        <Button color="secondary"  variant="contained" onClick={handleDecrement} >Pledge</Button>
+
+      </Stack>
+      </Card>
+      </Grid>
+    </Grid>
+    
     </>
   );
 }
